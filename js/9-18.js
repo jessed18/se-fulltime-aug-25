bc5dd47efa34c1c7c6fbb8b5984b3ede2f7d221f
jessed18 <jessedg27@gmail.com>
2025-09-18 12:30:24 -0700

    9/18 material so far

---
+++ js/9-18.js
@@ -0,0 +1,83 @@
+let person = {
+  name: "Jesse",
+  age: 21,
+  isStudent: true,
+
+  introduce: function() {
+    console.log("Hi, my name is " + this.name + " and I am " + this.age + " years old.");
+  },
+
+  celebrateBirthday: function() {
+    this.age++;
+    console.log("Happy Birthday! I am now " + this.age + " years old.");
+  }
+};
+
+console.log(person);        
+console.log(person.name);  
+console.log(person.age);    ; 
+
+
+person.introduce();       
+person.celebrateBirthday(); 
+
+
+function countItems(arr) {
+  let counts = {};
+
+  // count
+  for (let item of arr) {
+    if (counts[item]) {
+      counts[item]++;
+    } else {
+      counts[item] = 1;
+    }
+  }
+
+  // display results
+  for (let key in counts) {
+    console.log(key + " appears " + counts[key] + " times");
+  }
+}
+
+// collect input from prompt
+let items = [];
+while (true) {
+  let input = prompt("Enter a letter (or 0 to stop):");
+  if (input === "0") {
+    break;
+  }
+  items.push(input);
+}
+
+// return
+countItems(items);
+
+
+
+// simple object
+let student = {
+    name: "jesse",
+    age: "21",
+    course: "software engineering"
+};
+console.log("Name:" +student.name);
+conseole.log("Age:" +student.age);
+console.log("course:" + student.course);
+
+
+let car = {
+    brand: "toyota",
+    model: "corolla",
+    year: "2024",
+
+    start: function(){
+        console.log("The car has started");
+    }
+};
+
+console.log(car.brand);
+console.year(car.year);
+console.log(car.model);
+
+car.start();
--- js/function.js
+++ js/function.js
@@ -36,7 +36,27 @@ min([3,5,11,-3,5,89]);
 min([30,50,10,40,5,89]);
 min([300,500,100,400,50,890]);
 
-let items = [30,50,10,40,5,89];
+let items = [30,50,10,40,5,89, 5, 4, 5];
+
+function searchWithCount(numbers){
+    let input = parseint(prompt("Enter a number to search: "));
+    let count = 0;
+
+    for (let i = 0; i < numbers.length; i++){
+        if (numbers[i] == input) {
+            count++;
+        }
+    }
+
+    if (count > 0){
+        console.log(input + "is in the list " + count + "time(s).");
+    } else {
+        console.log(input + "is not in the list.");
+    }
+}
+
+searchWithCount(items);
+
 
 let a = sum(items);
 console.log("Sum is:" + a);
@@ -80,3 +100,92 @@ function minAndSecondMin(numbers) {
     console.log("Minimum:", min);
     console.log("Second minimum:", secondMin);
 }
+
+// simple function
+function greet(){
+    console.log("hello to js function");
+}
+greet();
+
+// math operations
+function addnumbers(a,b){
+    let sum = a+b;
+    console.log("the sum is" + sum);
+}
+
+addnumbers(5,10);
+addnumbers(6,8);
+addnumbers(7,8);
+
+function multiplyNumbers(a,b){
+    let product = a*b;
+    console.log("the product is" + product);
+}
+
+ multiplyNumbers(2,2);
+
+function divideNumbers(a,b){
+    let quotient  = a / b;
+    console.log("the quotient is" + quotient);
+}
+
+divideNumbers(12,4);
+
+function subtractNumbers(a,b){
+    let result  = a - b;
+    console.log("the difference is" + result);
+}
+
+ subtractNumbers(16,6);
+
+ // function with userInput
+
+ function mul(){
+    let num1 = parseInt(prompt("enter first number"));
+    let num2 = parseInt(prompt("enter second number"));
+    let result = num1 * num2;
+    console.log("the result is: " + result);
+ }
+
+ mul();
+
+ // another
+
+ function div(){
+    let num1 = parseInt(prompt("enter first number"));
+    let num2 = parseInt(prompt("enter second number"));
+    let result = num1 / num2;
+    console.log("the result is: " + result);
+ }
+
+ div();
+
+ // function that returns a value 
+
+ function square(num) {
+    return num*num;
+ }
+
+ let result = square(4);
+ console.log("the square is: "+result);
+
+ function runprogram() {
+    let cars = ["BMW", "Tesla", "Prius"];
+    let choice;
+
+    while(true){
+        choice = prompt("Enter a name to search for a car")
+        if(choice.toLowerCase()=== "quit")
+            console.log("exiting program... goodbye");
+            break; // stop loop
+    }
+
+    if(cars.includes(choice.toLowerCase)){
+        console.log(choice + "yes we have this car");
+    }
+    else {
+        console.log(choice+ "we don't have this car");
+    }
+ }
+
+ runprogram();
